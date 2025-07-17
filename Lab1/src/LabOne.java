8afb228b82b5d3b000f3bb6931a2629e2c651e8a
CelesTheAwesome <celesisawesome.mp4@gmail.com>
2025-07-17 18:35:28 -0400

    Add files via upload

---
+++ Lab1/src/LabOne.java
@@ -0,0 +1,7 @@
+
+public class LabOne{
+    public static void main (String[] args) {
+        System.out.println("CSC142 – Intro to Java");
+        System.out.println("In module 1 I learned how to use the print function, as well as how to build a for loop.");
+}
+}
